17dce5d268876445315de29959f644649748dbf6
KateUsacheva <usa4evi@gmail.com>
2019-03-06 22:46:45 +0300

    Ver1

---
+++ src/task_08/Schet.java
@@ -0,0 +1,9 @@
+package task_08;
+import java.util.Scanner; // импорт сканера
+public class Schet {
+    public static void main(String[] srgs) {
+        for (int i = 1; i <= 50; i++) {
+            System.out.println(i);
+        }
+    }
+}
