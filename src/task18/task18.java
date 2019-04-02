78b91ca68d424970c09c043456ddf6a802dbfb62
KateUsacheva <usa4evi@gmail.com>
2019-04-02 21:37:16 +0300

    надоделанная задача

---
+++ src/task18/task18.java
@@ -0,0 +1,43 @@
+package task18;
+/*Необходимо написать программу, которая будет выводить на экран текстовые данные из файла .txt,
+а потом в этот же файл перезаписывать текстовые данные, введенные вручную. Количество строк после перезаписи должно быть столько же,
+ сколько и в изначальном варианте.
+**/
+
+import java.io.*;
+
+public class task18 {
+    public static void main(String[] args) {
+        String S;
+        int CS = 0; /// кол-во строк
+        try {
+            BufferedReader br = new BufferedReader(new FileReader("D:\\task18.txt"));
+            while ((S = br.readLine()) != null) {
+                System.out.println(S) ;
+                CS++;
+            }
+            System.out.println("кол-во строк =" + CS + " Введите данные");
+        } catch (IOException exc) {
+            System.out.println("Ошибка ввода-вывода" + exc);
+        }
+        BufferedReader br = new BufferedReader (new InputStreamReader(System.in));
+        int CS2=0;
+        try (FileWriter fw = new FileWriter("D:\\task18.txt",false) )
+        {
+
+            do {
+                System.out.print(": ");
+                S= br.readLine();
+                if (S.compareTo("stop")==0) break; /// здесь должно быть сравнение строк было введено
+                S=S + "\r\n";
+                CS2++;
+                fw.write(S);
+            }
+            while (S.compareTo("stop") !=0);
+
+        } catch (IOException exc)
+        {
+            System.out.println("Неверное количество строк");
+        }
+    }
+}
